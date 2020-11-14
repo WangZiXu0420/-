1135f6b6cc55b0239ea79fdf8e19853d872df957
WangZiXu0420 <74420222+WangZiXu0420@users.noreply.github.com>
2020-11-14 23:38:28 +0800

    Add files via upload

---
+++ DataBase_manager.py
@@ -0,0 +1,71 @@
+# Database manager for controlling the database
+import json
+
+import pandas as pd
+from sqlalchemy import create_engine
+import time
+
+class DatabaseManager(object):
+
+    # init a database named db_name
+    def __init__(self, db_name):
+        # self.engine is used for the pandas
+        sql = 'sqlite:///%s' % db_name
+        self.engine = create_engine(sql)
+
+    def set_tablename(self, tablename):
+        self.table_name = tablename
+
+
+    # store json data to the sql , and return true
+    def store_json(self, data):
+        data_id = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        tmp = json.loads(data)
+        frame = pd.DataFrame([(tmp['name'][0], tmp['title'][0], tmp['tel'][0], tmp['email'][0], tmp['comp'][0],
+                               tmp['addr'][0], data_id)],
+                             columns=['name', 'title', 'tel', 'email', 'comp', 'addr', 'id'])
+        frame.to_sql(self.table_name, con=self.engine, index=False, if_exists='append')
+
+        return True
+
+    # stor dict data to the sql , and return true
+    def store_dict(self, data):
+        # print(data)
+        data_id = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        frame = pd.DataFrame(
+            [(data['name'], data['title'], data['tel'], data['email'], data['comp'],
+              data['addr'], data_id)],
+            columns=['name', 'title', 'tel', 'email', 'comp', 'addr', 'id'])
+        frame.to_sql(self.table_name, con=self.engine, index=False, if_exists='append')
+        return data_id
+
+    # update a data by id
+    def update(self, data):
+        sql = "update %s set name='%s' , title='%s', tel='%s', email='%s', comp='%s', addr='%s' where id=%s" % (
+            self.table_name, data['name'], data['title'], data['tel'], data['email'], data['comp'], data['addr'], data['id'])
+        self.engine.execute(sql)
+
+    # delete a data by id
+    def delete(self, id):
+        sql = "delete from %s where id = %s" % (self.table_name, id)
+        self.engine.execute(sql)
+
+
+    # fuzzy search
+    def search(self, attrbute, s):
+        sql = "select * from %s where ( %s like '%s')" % (self.table_name, attrbute, '%'+s+'%')
+        lines = pd.read_sql_query(sql, con=self.engine, index_col=None)
+        return lines
+
+    def get_all(self):
+        lines = pd.read_sql(self.table_name, self.engine, columns=['name', 'title', 'tel', 'email', 'comp', 'addr', 'id'])
+        return lines
+
+
+if __name__ == '__main__':
+    database = DatabaseManager('system.db')
+    database.set_tablename('peoples')
+    print(database.get_all())
+    data = {'name': 'name', 'title': 'title', 'tel': 'tel', 'email': 'email', 'comp': 'comp', 'addr': 'addr', 'id': '20201114223505'}
+    print(database.search('name', 'aaa'))
+    #print(database.get_all())
---
+++ first_ui_program.py
@@ -0,0 +1,80 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from otherclass import *
+from Data_Wash import *
+from PyQt5.QtSql import *
+from first_edtion import *
+from query_page_op import *
+from DataBase_manager import *
+
+class UI_program_main_op(QWidget):
+    def __init__(self, database_manager, parent=None):
+        super(UI_program_main_op, self).__init__(parent)
+        self.ui_main = Ui_main_Form()
+        self.ui_main.setupUi(self)
+        self.database_manager = database_manager
+
+
+    def load_picture(self):
+        print("load--file")
+        fname, _ = QFileDialog.getOpenFileName(self, '选择图片', 'c:\\', 'Image files(*.jpg *.gif *.png)')
+        if fname == "":
+            return
+        pixmax = QPixmap(fname)
+        scarepixmap = pixmax.scaled(100, 100, aspectRatioMode=Qt.KeepAspectRatio)
+        self.ui_main.label.setPixmap(scarepixmap)
+        img = Image.open(fname)
+        # 通过OCR类获取到字典
+        json_result = OCR('5aba1bb3-ffef-4c5b-ad93-974d406edd49', img).remote_call()
+        if json_result['code'] == '434':
+            self.ui_main.textBrowser.setText("名片识别今天20次已经用完！\n")
+            return
+        # result = json.loads(json_result)
+        dict_result = Data_Wash().wash_op(json_result)
+        #
+        self.database_manager.store_dict(dict_result)
+
+        ms0 = '成功生成信息！\n'
+        ms1 = '*姓名:   %s' % dict_result['name'] + '\n'
+        ms2 = '*头衔:   %s' % dict_result['title'] + '\n'
+        ms3 = '*公司:   %s' % dict_result['comp'] + '\n'
+        ms4 = '*地址:   %s' % dict_result['addr'] + '\n'
+        ms5 = '*电话:   %s' % dict_result['mobile'] + '\n'
+        ms6 = '*手机:   %s' % dict_result['tel'] + '\n'
+        ms7 = '*email:  %s' % dict_result['email'] + '\n'
+        ms8 = '*传真:   %s' % dict_result['fax'] + '\n'
+        result = ms0 + ms1 + ms2 + ms3 + ms4 + ms5 + ms6 + ms7 + ms8
+        print(result)
+        self.ui_main.textBrowser.setText(result)
+
+    def query_person(self):
+        print("查询联系人方法")
+        #self.query_app = QApplication(sys.argv)
+        #启动查询UI程序，并向其传入数据库对象
+        self.query_program = UI_program_query_person_op(self.database_manager)
+        self.query_program.show()
+        #sys.exit(self.query_app.exec_())
+
+    def visualize_data(self):
+        print("数据可视化发方法")
+
+
+
+    def exit_program(self):
+        sender = self.sender()
+        app = QApplication.instance()
+        app.quit()
+
+
+
+
+if __name__ == '__main__':
+    database_manager = DatabaseManager('system.db')
+    database_manager.set_tablename('peoples')
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+    app = QApplication(sys.argv)
+    program = UI_program_main_op(database_manager)
+    program.show()
+    sys.exit(app.exec_())
